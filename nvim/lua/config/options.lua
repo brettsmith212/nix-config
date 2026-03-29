5afeaedfd9549f897928a002a1934cab811689da
Brett Smith <brettsmith212@gmail.com>
2026-03-29 14:15:19 -0700

    clipboard

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,1 +1,15 @@
 vim.opt.relativenumber = false
+
+if os.getenv("SSH_CONNECTION") then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
