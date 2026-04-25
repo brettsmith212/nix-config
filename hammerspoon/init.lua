ace222753daffcb00c5a9084b8033579a4aa4fee
Brett Smith <brettsmith212@gmail.com>
2026-04-24 18:09:11 -0700

    update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,16 +1,5 @@
 local prefix = hs.hotkey.modal.new()
 
--- Only activate prefix in Safari
-hs.hotkey.bind({"ctrl"}, "a", function()
-  if hs.application.frontmostApplication():name() == "Safari" then
-    prefix:enter()
-    hs.alert.show("Ctrl-A", 0.5)
-  else
-    -- pass through (e.g. beginning-of-line in terminals/inputs)
-    hs.eventtap.keyStroke({"ctrl"}, "a", 0)
-  end
-end)
-
 prefix:bind({}, "n", function()
   hs.eventtap.keyStroke({"cmd", "shift"}, "]")  -- next tab
   prefix:exit()
@@ -22,3 +11,29 @@ prefix:bind({}, "p", function()
 end)
 
 prefix:bind({}, "escape", function() prefix:exit() end)
+
+-- Ctrl-A hotkey, only enabled while Safari is frontmost
+local ctrlA = hs.hotkey.new({"ctrl"}, "a", function()
+  prefix:enter()
+  hs.alert.show("Ctrl-A", 0.5)
+end)
+
+local function updateForApp(appName)
+  if appName == "Safari" then
+    ctrlA:enable()
+  else
+    ctrlA:disable()
+    prefix:exit()
+  end
+end
+
+appWatcher = hs.application.watcher.new(function(name, eventType, app)
+  if eventType == hs.application.watcher.activated then
+    updateForApp(name)
+  end
+end)
+appWatcher:start()
+
+-- Initial state
+local front = hs.application.frontmostApplication()
+updateForApp(front and front:name() or "")
